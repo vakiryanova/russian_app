af3ce0d9fef3f2fe2d2d1746104cc549dae631e1
Виктория Кирьянова <vakiryanova@miem.hse.ru>
2021-03-25 02:18:43 +0300

    добавлена страница с тестами

--- exercises/urls.py
+++ exercises/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', landing_page, name='landing_page'),
     path('exercises', exercises, name='exercises'),
     path('exercises/orthoepy', orthoepy_page, name='orthoepy'),
-    path('exercises/spelling', spelling_page, name='spelling'),
+    url(r'exercises/spelling', spelling_page, name='spelling'),
     path('exercises/punctuation', punctuation_page, name='punctuation'),
     url(r'exercises/end', practice_end),
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -12,24 +12,102 @@ models_ = {
 
 # Create your views here.
 
+# служебный функции
+def get_amount_and_level(request):
+	try:
+		amount = int(request.GET.get('amount'))
+	except:
+		amount = None
+
+	try:
+		level = int(request.GET.get('level'))/100
+	except:
+		level = None
+	return amount, level
+
+
+# views
 def landing_page(request):
 	return render(request, 'front/about.html')
 
 def exercises(request):
-	return render(request, 'front/tests.html')
+	try:
+		_type = request.GET.get('type')
+	except:
+		_type = None
+	print(_type)
+
+	return render(request, 'front/tests.html', {'type': _type})
 
 def orthoepy_page(request):
-	words = Orthoepy.objects.all()
+	amount, level = get_amount_and_level(request)
+
+	if level != None:
+		all_words = Orthoepy.objects.all()
+		words = []
+		for word in all_words:
+			print(word)
+			if word.total_count == 0:
+				if 0.5 >= level:
+					words.append(word)
+			elif word.mistakes/word.total_count >= level:
+				words.append(word)
+			if amount!=None and len(words)==amount:
+				break
+		if len(words) == 0:
+			return redirect("/exercises?type=_orthoepy")
+	else:
+		words = Orthoepy.objects.all()
+		print(words)
+
 	words_serialized = OrthoepySerializer(words, many=True)
 	return render(request, 'front/orthoepy.html', {'data': words_serialized.data})
 
 def spelling_page(request):
-	words = Spelling.objects.all()
+	amount, level = get_amount_and_level(request)
+
+	if level != None:
+		all_words = Spelling.objects.all()
+		words = []
+		for word in all_words:
+			print(word)
+			if word.total_count == 0:
+				if 0.5 >= level:
+					words.append(word)
+			elif word.mistakes/word.total_count >= level:
+				words.append(word)
+			if amount!=None and len(words)==amount:
+				break
+		if len(words) == 0:
+			return redirect("/exercises?type=_spelling")
+	else:
+		words = Spelling.objects.all()
+		print(words)
 	words_serialized = SpellingSerializer(words, many=True)
 	return render(request, 'front/spelling.html', {'data': words_serialized.data})
 
 def punctuation_page(request):
-	words = Punctuation.objects.all()
+	amount, level = get_amount_and_level(request)
+
+	if level != None:
+		all_words = Punctuation.objects.all()
+		words = []
+		for word in all_words:
+			print(word)
+			if word.total_count == 0:
+				if 0.5 >= level:
+					words.append(word)
+			elif word.mistakes/word.total_count >= level:
+				words.append(word)
+			if amount!=None and len(words)==amount:
+				break
+		if len(words) == 0:
+			return redirect("/exercises?type=_punctuation")
+			# return render(request, 'front/tests.html', {'type': 'no records'})
+	else:
+		words = Punctuation.objects.all()
+		print(words)
+
 	words_serialized = PunctuationSerializer(words, many=True)
 	return render(request, 'front/punctuation.html', {'data': words_serialized.data})
 
